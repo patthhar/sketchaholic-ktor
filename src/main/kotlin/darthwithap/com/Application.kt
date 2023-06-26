d16270dac549dcc2738b2c03f97c09c0a00e87b0
patthhar <parthdroidworld@gmail.com>
2023-06-26 14:04:09 +0530

    added intercept to check if client has a session id, if not then assign one

--- src/main/kotlin/darthwithap/com/Application.kt
+++ src/main/kotlin/darthwithap/com/Application.kt
@@ -2,12 +2,31 @@ package darthwithap.com
 
 import io.ktor.server.application.*
 import darthwithap.com.plugins.*
+import darthwithap.com.session.DrawingSession
+import io.ktor.server.application.ApplicationCallPipeline.ApplicationPhase.Plugins
+import io.ktor.server.sessions.*
+import io.ktor.util.*
 
 fun main(args: Array<String>): Unit =
   io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+  install(Sessions) {
+    cookie<DrawingSession>("SESSION")
+  }
+  intercept(Plugins) {
+    if (call.sessions.get<DrawingSession>() == null) {
+      val clientId = call.parameters["client_id"] ?: ""
+      call.sessions.set(
+        DrawingSession(
+          clientId = clientId,
+          sessionId = generateNonce()
+        )
+      )
+    }
+  }
+
   configureSerialization()
   configureSockets()
   configureMonitoring()
