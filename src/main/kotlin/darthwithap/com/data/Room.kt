899f389e5df586bcf26e476a1c565e98687c02f6
patthhar <parthdroidworld@gmail.com>
2023-06-27 17:42:11 +0530

    addPlayer functionality added in Room class

--- src/main/kotlin/darthwithap/com/data/Room.kt
+++ src/main/kotlin/darthwithap/com/data/Room.kt
@@ -1,5 +1,7 @@
 package darthwithap.com.data
 
+import darthwithap.com.data.models.Announcement
+import darthwithap.com.gson
 import io.ktor.websocket.*
 import kotlinx.coroutines.isActive
 
@@ -35,6 +37,34 @@ class Room(
         Phase.ENDED -> waitingForPlayers()
       }
     }
+  }
+
+  suspend fun addPlayer(
+    clientId: String,
+    username: String,
+    socket: WebSocketSession
+  ): Player {
+    val player = Player(username, socket, clientId)
+    players += player
+
+    if (players.size == 1) {
+      phase = Phase.WAITING_FOR_PLAYERS
+    } else if (players.size == 2 && phase == Phase.WAITING_FOR_PLAYERS) {
+      phase = Phase.WAITING_FOR_START
+      players = players.shuffled()
+    } else if (phase == Phase.WAITING_FOR_START && players.size == maxPlayers) {
+      phase = Phase.NEW_ROUND
+      players = players.shuffled()
+    }
+
+    val announcement = Announcement(
+      "$username joined",
+      System.currentTimeMillis(),
+      Announcement.TYPE_PLAYER_JOINED
+    )
+    broadcast(gson.toJson(announcement))
+
+    return player
   }
 
   suspend fun broadcast(message: String) {
