ab7f848f20285fdf662d0ea648c8a6c9313535cd
patthhar <parthdroidworld@gmail.com>
2023-06-27 17:11:28 +0530

    route handler for DrawData ws added

--- src/main/kotlin/darthwithap/com/routes/WebSocketRoutes.kt
+++ src/main/kotlin/darthwithap/com/routes/WebSocketRoutes.kt
@@ -1,18 +1,40 @@
 package darthwithap.com.routes
 
 import com.google.gson.JsonParser
+import darthwithap.com.data.Room
 import darthwithap.com.data.models.BaseModel
 import darthwithap.com.data.models.ChatMessage
+import darthwithap.com.data.models.DrawData
 import darthwithap.com.gson
+import darthwithap.com.server
 import darthwithap.com.session.DrawingSession
-import darthwithap.com.utils.Constants
+import darthwithap.com.utils.Constants.TYPE_CHAT_MESSAGE
+import darthwithap.com.utils.Constants.TYPE_DRAW_DATA
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
 
-fun Route.standardWebSocketRoute(
+fun Route.webSocketRouting() {
+  route("/ws/draw") {
+    standardWebSocket { socket, clientId, message, payload ->
+      when(payload) {
+        is DrawData -> {
+          val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+          if (room.phase == Room.Phase.GAME_RUNNING) {
+            room.broadcastToAllExcept(message, clientId)
+          }
+        }
+        is ChatMessage -> {
+
+        }
+      }
+    }
+  }
+}
+
+fun Route.standardWebSocket(
   handleFrame: suspend (
     socket: DefaultWebSocketServerSession,
     clientId: String,
@@ -32,7 +54,8 @@ fun Route.standardWebSocketRoute(
           val message = frame.readText()
           val jsonObject = JsonParser.parseString(message).asJsonObject
           val type = when (jsonObject.get("type").asString) {
-            Constants.TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+            TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+            TYPE_DRAW_DATA -> DrawData::class.java
             else -> BaseModel::class.java
           }
           val payload = gson.fromJson(message, type)
--- src/main/kotlin/darthwithap/com/utils/Constants.kt
+++ src/main/kotlin/darthwithap/com/utils/Constants.kt
@@ -3,4 +3,5 @@ package darthwithap.com.utils
 object Constants {
   const val MAX_ROOM_SIZE = 10;
   const val TYPE_CHAT_MESSAGE = "TYPE_CHAT_MESSAGE"
+  const val TYPE_DRAW_DATA = "TYPE_DRAW_DATA"
 }
