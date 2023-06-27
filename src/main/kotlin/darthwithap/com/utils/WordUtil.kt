c8eac9540d9a379b4b896da2a7336d1bcf0b6fdd
patthhar <parthdroidworld@gmail.com>
2023-06-28 00:40:14 +0530

    wordlist added & WordListUtil class added

---
+++ src/main/kotlin/darthwithap/com/utils/WordUtil.kt
@@ -0,0 +1,35 @@
+package darthwithap.com.utils
+
+import java.io.File
+
+val words = readWordList("src/main/resources/wordlist.txt")
+
+fun readWordList(filename: String): List<String> {
+  val inputStream = File(filename).inputStream()
+  val words = mutableListOf<String>()
+  inputStream.bufferedReader().forEachLine {
+    words.add(it)
+  }
+  return words
+}
+
+fun getRandomWords(amount: Int = 3): List<String> {
+  var currAmount = 0
+  val result = mutableListOf<String>()
+  if (words.isNotEmpty()){
+    while (currAmount < amount) {
+      val word = words.random()
+      if (!result.contains(word)) {
+        result.add(word)
+        currAmount++
+      }
+    }
+  }
+  return result
+}
+
+fun String.transformToUnderscores() {
+  toCharArray().map {
+    if (it != ' ') '_' else ' '
+  }.joinToString(" ")
+}
